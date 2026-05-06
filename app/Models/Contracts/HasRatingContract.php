b4b61ea12bc54468b4edc4b6ce3e636a092aa646
marco76tv <marco76tv@users.noreply.github.com>
2026-05-06 10:29:50 +0000

    Check & fix styling

--- app/Models/Contracts/HasRatingContract.php
+++ app/Models/Contracts/HasRatingContract.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Models\Contracts;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use Modules\Rating\Models\Rating;
 
 /**
  * --.
  */
 interface HasRatingContract
 {
-    /**
-     * @return MorphToMany
-     */
     public function ratings(): MorphToMany;
 }
 
