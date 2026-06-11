ed6ed172c49496afe99661a60fe42847f60a7b91
Malebestia <malebestia@yahoo.it>
2026-06-11 10:00:31 +0200

    .

--- database/factories/RatingFactory.php
+++ database/factories/RatingFactory.php
@@ -7,6 +7,9 @@ namespace Modules\Rating\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\Rating\Models\Rating;
 
+/**
+ * @extends Factory<Rating>
+ */
 class RatingFactory extends Factory
 {
     /**
