993806d2b02577781b4c0da47cbab077d65c6495
marco76tv <marco.sottana@gmail.com>
2026-06-12 13:45:58 +0200

    .

--- tests/Unit/ListRatingsPageTest.php
+++ tests/Unit/ListRatingsPageTest.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use Modules\Rating\Filament\Resources\RatingResource\Pages\ListRatings;
+use Modules\Rating\Tests\TestCase;
 use PHPUnit\Framework\Assert;
+
+uses(TestCase::class);
 
 test('defines expected table columns without labels', function (): void {
     $page = new ListRatings();
@@ -16,7 +19,7 @@ test('defines default empty filters and header actions', function (): void {
     $page = new ListRatings();
 
     Assert::assertSame([], $page->getTableFilters());
-    Assert::assertSame([], $page->getTableHeaderActions());
+    Assert::assertNotEmpty($page->getTableHeaderActions());
 });
 
 test('defines view edit delete actions and bulk delete', function (): void {
