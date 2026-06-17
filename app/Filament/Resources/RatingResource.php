233ac746bc27a06ae2d3c273796394d16412bd49
marco76tv <marco76tv@users.noreply.github.com>
2026-06-17 06:00:37 +0000

    Check & fix styling

--- app/Filament/Resources/RatingResource.php
+++ app/Filament/Resources/RatingResource.php
@@ -4,23 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Filament\Resources;
 
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Schemas\Components\Section;
-use Filament\Support\Components\Component;
-use Modules\Rating\Enums\RuleEnum;
-use Modules\Rating\Filament\Resources\RatingResource\Pages\CreateRating;
-use Modules\Rating\Filament\Resources\RatingResource\Pages\EditRating;
-use Modules\Rating\Filament\Resources\RatingResource\Pages\ListRatings;
 use Modules\Rating\Models\Rating;
-use Modules\Xot\Filament\Resources\XotBaseResource;
 
 class RatingResource extends BaseRatingResource
 {
     protected static ?string $model = Rating::class;
-
-    
 }
--- app/Filament/Resources/RatingResource/Pages/ListRatings.php
+++ app/Filament/Resources/RatingResource/Pages/ListRatings.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Filament\Resources\RatingResource\Pages;
 
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
 use Modules\Rating\Filament\Resources\RatingResource;
-use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListRatings extends BaseListRatings
 {
     protected static string $resource = RatingResource::class;
-
-    
 }
