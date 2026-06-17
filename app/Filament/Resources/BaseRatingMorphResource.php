ee8449e74fb790ca22ebbc557af469a01bd24c11
marco76tv <marco.sottana@gmail.com>
2026-06-17 08:00:12 +0200

    .

---
+++ app/Filament/Resources/BaseRatingMorphResource.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Rating\Filament\Resources;
+
+use Modules\Rating\Models\RatingMorph;
+use Modules\Xot\Filament\Resources\XotBaseResource;
+
+abstract class BaseRatingMorphResource extends XotBaseResource
+{
+    protected static ?string $model = RatingMorph::class;
+
+    public static function getFormSchema(): array
+    {
+        return [
+            // Campi del form
+        ];
+    }
+
+    
+}
--- app/Filament/Resources/RatingResource.php
+++ app/Filament/Resources/RatingResource.php
@@ -18,44 +18,9 @@ use Modules\Rating\Filament\Resources\RatingResource\Pages\ListRatings;
 use Modules\Rating\Models\Rating;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 
-class RatingResource extends XotBaseResource
+class RatingResource extends BaseRatingResource
 {
     protected static ?string $model = Rating::class;
 
-    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    /**
-     * @return array<string, Component>
-     */
-    public static function getFormSchema(): array
-    {
-        return [
-            'extra_attributes.type' => TextInput::make('extra_attributes.type'),
-            'extra_attributes.anno' => TextInput::make('extra_attributes.anno'),
-            'title' => TextInput::make('title')->autofocus()->required(),
-            'color' => ColorPicker::make('color'),
-            'rule' => Radio::make('rule')->options(RuleEnum::class),
-            'flags' => Section::make()
-                ->schema([
-                    Toggle::make('is_disabled'),
-                    Toggle::make('is_readonly'),
-                ]),
-            'txt' => RichEditor::make('txt')->columnSpanFull(),
-        ];
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-        ];
-    }
-
-    public static function getPages(): array
-    {
-        return [
-            'index' => ListRatings::route('/'),
-            'create' => CreateRating::route('/create'),
-            'edit' => EditRating::route('/{record}/edit'),
-        ];
-    }
+    
 }
