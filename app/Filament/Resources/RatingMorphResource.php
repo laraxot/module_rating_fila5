5633488be1cf05d99f51da2ab7c07f015fafb69f
marco76tv <marco76tv@users.noreply.github.com>
2026-06-17 06:00:37 +0000

    Check & fix styling

--- app/Filament/Resources/RatingMorphResource.php
+++ app/Filament/Resources/RatingMorphResource.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Filament\Resources;
 
-use Modules\Rating\Filament\Resources\RatingMorphResource\Pages\CreateRatingMorph;
-use Modules\Rating\Filament\Resources\RatingMorphResource\Pages\EditRatingMorph;
-use Modules\Rating\Filament\Resources\RatingMorphResource\Pages\ListRatingMorphs;
 use Modules\Rating\Models\RatingMorph;
-use Modules\Xot\Filament\Resources\XotBaseResource;
 
 class RatingMorphResource extends BaseRatingMorphResource
 {
     protected static ?string $model = RatingMorph::class;
-
-    
 }
