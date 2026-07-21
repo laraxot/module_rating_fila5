e73d8e96f8ac851539d37b5631a1bee6ebfcf5bf
marco76tv <marco.sottana@gmail.com>
2026-07-21 15:27:07 +0200

    .

--- app/Filament/Resources/RatingMorphResource/Pages/ListRatingMorphs.php
+++ app/Filament/Resources/RatingMorphResource/Pages/ListRatingMorphs.php
@@ -11,7 +11,7 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListRatingMorphs extends XotBaseListRecords
 {
-    protected static string $resource = RatingMorphResource::class;
+    public static string $resource = RatingMorphResource::class;
 
     /**
      * @return array<string, mixed>
