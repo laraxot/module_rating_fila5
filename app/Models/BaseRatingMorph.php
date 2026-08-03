88779bb578c36f30896017762233d83fbe08fae3
marco76tv <marco76tv@users.noreply.github.com>
2026-08-03 11:31:37 +0000

    Check & fix styling

--- app/Models/BaseRatingMorph.php
+++ app/Models/BaseRatingMorph.php
@@ -17,32 +17,33 @@ use Modules\Xot\Datas\XotData;
 /**
  * Modules\Rating\Models\RatingMorph.
  *
-<<<<<<< HEAD
- * @property int $id
- * @property bool $is_winner
- * @property string|null $post_type
- * @property int|null $post_id
- * @property string|null $related_type
- * @property int|null $related_id
-=======
+ * <<<<<<< HEAD
+ *
  * @property int         $id
  * @property bool        $is_winner
  * @property string|null $post_type
  * @property int|null    $post_id
  * @property string|null $related_type
  * @property int|null    $related_id
->>>>>>> laraxot/dev
+ *                                     =======
+ * @property int         $id
+ * @property bool        $is_winner
+ * @property string|null $post_type
+ * @property int|null    $post_id
+ * @property string|null $related_type
+ * @property int|null    $related_id
+ *                                     >>>>>>> laraxot/dev
  * @property Rating|null $rating
  * @property string|null $created_by
  * @property string|null $updated_by
  * @property string|null $deleted_by
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
-<<<<<<< HEAD
- * @property int|null $auth_user_id
-=======
+ *                                     <<<<<<< HEAD
  * @property int|null    $auth_user_id
->>>>>>> laraxot/dev
+ *                                     =======
+ * @property int|null    $auth_user_id
+ *                                     >>>>>>> laraxot/dev
  *
  * @method static Builder|RatingMorph newModelQuery()
  * @method static Builder|RatingMorph newQuery()
@@ -62,15 +63,15 @@ use Modules\Xot\Datas\XotData;
  *
  * @property string|null $user_id
  * @property string|null $model_type
-<<<<<<< HEAD
- * @property int|null $model_id
- * @property int $rating_id
- * @property int|null $value
-=======
+ *                                   <<<<<<< HEAD
  * @property int|null    $model_id
  * @property int         $rating_id
  * @property int|null    $value
->>>>>>> laraxot/dev
+ *                                   =======
+ * @property int|null    $model_id
+ * @property int         $rating_id
+ * @property int|null    $value
+ *                                   >>>>>>> laraxot/dev
  * @property string|null $note
  * @property string|null $deleted_at
  *
@@ -83,35 +84,36 @@ use Modules\Xot\Datas\XotData;
  * @method static Builder|RatingMorph whereUserId($value)
  * @method static Builder|RatingMorph whereValue($value)
  *
-<<<<<<< HEAD
- * @property Model|Eloquent $model
- * @property Model|null $profile
- * @property UserContract|null $user
- * @property string $reward
-=======
+ * <<<<<<< HEAD
+ *
  * @property Model|\Eloquent   $model
  * @property Model|null        $profile
  * @property UserContract|null $user
  * @property string            $reward
->>>>>>> laraxot/dev
+ *                                      =======
+ * @property Model|\Eloquent   $model
+ * @property Model|null        $profile
+ * @property UserContract|null $user
+ * @property string            $reward
+ *                                      >>>>>>> laraxot/dev
  *
  * @method static Builder|RatingMorph whereReward($value)
  *
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
-<<<<<<< HEAD
- * @property string $sum_credit_yes
- * @property string $sum_credit_no
- * @property int $count_credit_yes
- * @property int $count_credit_no
- * @property string $percentage
-=======
+ *                                                  <<<<<<< HEAD
  * @property string               $sum_credit_yes
  * @property string               $sum_credit_no
  * @property int                  $count_credit_yes
  * @property int                  $count_credit_no
  * @property string               $percentage
->>>>>>> laraxot/dev
+ *                                                  =======
+ * @property string               $sum_credit_yes
+ * @property string               $sum_credit_no
+ * @property int                  $count_credit_yes
+ * @property int                  $count_credit_no
+ * @property string               $percentage
+ *                                                  >>>>>>> laraxot/dev
  *
  * @method static Builder<static>|RatingMorph whereCountCreditNo($value)
  * @method static Builder<static>|RatingMorph whereCountCreditYes($value)
