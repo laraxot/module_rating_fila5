511634a3955e965eb9f1ab526156178d871ad14d
Marco Xot <marco76tv@hotmail.com>
2026-08-05 22:50:22 +0200

    .

--- database/migrations/2026_03_27_000002_add_percentage_to_rating_morph_table_on_rating_connection.php
+++ database/migrations/2026_03_27_000002_add_percentage_to_rating_morph_table_on_rating_connection.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD
 return new class extends Migration
 {
-=======
-return new class extends Migration {
->>>>>>> laraxot/dev
     /**
      * Run the migrations.
      */
