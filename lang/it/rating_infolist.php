a5fd7f6b2a8bd75a9fa326cdaf3cbbc55c3c16aa
Marco Xot <marco76tv@hotmail.com>
2026-09-07 22:46:04 +0200

    .

---
+++ lang/it/rating_infolist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'id' => ['label' => 'id'],
+        'title' => ['label' => 'title'],
+        'color' => ['label' => 'color'],
+        'txt' => ['label' => 'txt'],
+        'rule' => ['label' => 'rule'],
+        'is_disabled' => ['label' => 'is_disabled'],
+        'is_readonly' => ['label' => 'is_readonly'],
+        'order_column' => ['label' => 'order_column'],
+        'slug' => ['label' => 'slug'],
+    ],
+];
