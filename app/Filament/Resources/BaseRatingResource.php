bbea8274050c66907b9d81529e869d6995cd8043
marco76tv <marco76tv@users.noreply.github.com>
2026-09-21 09:49:21 +0000

    Check & fix styling

--- app/Filament/Resources/BaseRatingResource.php
+++ app/Filament/Resources/BaseRatingResource.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Filament\Resources;
 
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Schemas\Components\Section;
-use Modules\Rating\Enums\RuleEnum;
 use Modules\Rating\Models\Rating;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 
