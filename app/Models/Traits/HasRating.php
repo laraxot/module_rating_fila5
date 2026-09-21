a3675c01bd123ec03d77d919206697ee4ef74a93
marco76tv <marco76tv@users.noreply.github.com>
2026-09-21 10:01:47 +0000

    Check & fix styling

--- app/Models/Traits/HasRating.php
+++ app/Models/Traits/HasRating.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Models\Traits;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Modules\Rating\Models\Rating;
