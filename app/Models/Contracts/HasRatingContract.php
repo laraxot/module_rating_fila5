d78f3c0d78e99fb622a2d6cfe5141353a78b02d9
marco76tv <marco76tv@users.noreply.github.com>
2026-09-21 10:06:52 +0000

    Check & fix styling

--- app/Models/Contracts/HasRatingContract.php
+++ app/Models/Contracts/HasRatingContract.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Modules\Rating\Models\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Modules\Rating\Models\Rating;
 
