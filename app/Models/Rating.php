9ea05b4f09d4f57a42fd8adb652a75bbf9e2f81e
marco76tv <marco76tv@users.noreply.github.com>
2026-09-24 20:52:54 +0000

    Check & fix styling

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -99,14 +99,16 @@ use Spatie\SchemalessAttributes\SchemalessAttributes;
  *
  * @property ProfileContract|null $deleter
  *
-<<<<<<< HEAD
-<<<<<<< HEAD
+ * <<<<<<< HEAD
+ * <<<<<<< HEAD
+ *
  * @method static Builder<static>|Rating whereExtraAttributes($value)
  *
-=======
->>>>>>> e8cf105 (Check & fix styling)
-=======
->>>>>>> 77b9106 (.)
+ * =======
+ * >>>>>>> e8cf105 (Check & fix styling)
+ * =======
+ * >>>>>>> 77b9106 (.)
+ *
  * @mixin Eloquent
  */
 class Rating extends BaseRating
