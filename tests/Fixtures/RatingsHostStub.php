226f5f903fe375c39e31964319c2c4a05ef50ffa
marco76tv <marco76tv@users.noreply.github.com>
2026-09-24 10:42:14 +0000

    Check & fix styling

--- tests/Fixtures/RatingsHostStub.php
+++ tests/Fixtures/RatingsHostStub.php
@@ -38,12 +38,14 @@ class RatingsHostStub extends AbstractRatingsHost
     /**
      * @template TRelatedModel of Model
      *
-<<<<<<< .merge_file_m4CG3P
-     * @param  class-string<TRelatedModel>  $related
-=======
+     * <<<<<<< .merge_file_m4CG3P
+     *
+     * @param class-string<TRelatedModel> $related
+     *                                             =======
      * @param class-string<TRelatedModel> $related
      *
->>>>>>> .merge_file_FlcIoI
+     * >>>>>>> .merge_file_FlcIoI
+     *
      * @return MorphToMany<TRelatedModel, $this, MorphPivot, 'pivot'>
      */
     public function morphToManyX(
@@ -116,12 +118,14 @@ class RatingsHostStub extends AbstractRatingsHost
     /**
      * @template TRelatedModel of Model
      *
-<<<<<<< .merge_file_m4CG3P
-     * @param  class-string<TRelatedModel>  $related
-=======
+     * <<<<<<< .merge_file_m4CG3P
+     *
+     * @param class-string<TRelatedModel> $related
+     *                                             =======
      * @param class-string<TRelatedModel> $related
      *
->>>>>>> .merge_file_FlcIoI
+     * >>>>>>> .merge_file_FlcIoI
+     *
      * @return HasMany<TRelatedModel, $this>
      */
     public function hasMany($related, $foreignKey = null, $localKey = null)
