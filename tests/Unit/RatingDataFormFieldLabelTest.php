c564a2e3661e1cbe65d3f2ae9f7c0fea2181f19f
marco76tv <marco76tv@users.noreply.github.com>
2026-09-24 07:07:43 +0000

    Check & fix styling

--- tests/Unit/RatingDataFormFieldLabelTest.php
+++ tests/Unit/RatingDataFormFieldLabelTest.php
@@ -13,14 +13,14 @@ uses(TestCase::class);
 
 describe('RatingData::formFieldLabel', function (): void {
     test('preferisce txt a title e toglie HTML', function (): void {
-        $rating = new Rating;
+        $rating = new Rating();
         $rating->forceFill(['id' => 1, 'title' => 'Ruolo', 'txt' => 'Ruolo <b>organizzativo</b>']);
 
         Assert::assertSame('Ruolo organizzativo', RatingData::formFieldLabel($rating));
     });
 
     test('usa title se txt assente', function (): void {
-        $rating = new Rating;
+        $rating = new Rating();
         $rating->forceFill(['id' => 2, 'title' => 'Criterio']);
 
         Assert::assertSame('Criterio', RatingData::formFieldLabel($rating));
