a27c01833ae62efebb363aa15fd098963ffc24c2
marco76tv <marco76tv@users.noreply.github.com>
2026-09-25 07:00:48 +0000

    Check & fix styling

--- app/Models/Contracts/RatingContract.php
+++ app/Models/Contracts/RatingContract.php
@@ -38,18 +38,8 @@ use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
  *
  * @phpstan-require-extends Model
  *
-<<<<<<< .merge_file_tCmmJ1
- * @property int         $id
- * @property int|null    $parent_id
- * @property string|null $title
- * @property string|null $txt
- * @property string|null $slug
- * @property bool|null   $is_readonly
- * @property bool|null   $is_disabled
- * @property int|null    $order_column
- * @property Collection<int, Model> $children
- * @property RuleEnum    $rule
-=======
+ * <<<<<<< .merge_file_tCmmJ1
+ *
  * @property int                    $id
  * @property int|null               $parent_id
  * @property string|null            $title
@@ -60,7 +50,18 @@ use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
  * @property int|null               $order_column
  * @property Collection<int, Model> $children
  * @property RuleEnum               $rule
->>>>>>> .merge_file_UJNWcp
+ *                                                =======
+ * @property int                    $id
+ * @property int|null               $parent_id
+ * @property string|null            $title
+ * @property string|null            $txt
+ * @property string|null            $slug
+ * @property bool|null              $is_readonly
+ * @property bool|null              $is_disabled
+ * @property int|null               $order_column
+ * @property Collection<int, Model> $children
+ * @property RuleEnum               $rule
+ *                                                >>>>>>> .merge_file_UJNWcp
  */
 interface RatingContract extends HasRecursiveRelationshipsContract
 {
